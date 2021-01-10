0a72843c8519366b5b2485995fe7ac7345408d68
woooyoung <axdsw121@gmail.com>
2021-01-10 22:05:53 +0900

    MVC structure improvement

--- blog/src/com/sbs/java/blog/controller/ArticleController.java
+++ blog/src/com/sbs/java/blog/controller/ArticleController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.sbs.java.blog.dto.Article;
 import com.sbs.java.blog.service.ArticleService;
+import com.sbs.java.blog.util.Util;
 
 public class ArticleController extends Controller {
 	private ArticleService articleService;
@@ -20,9 +21,35 @@ public class ArticleController extends Controller {
 		switch (actionMethodName) {
 		case "list":
 			return doActionList(req, resp);
+		case "detail":
+			return doActionDetail(req, resp);
+		case "doWrite":
+			return doActionDoWrite(req, resp);
 		}
 
 		return "";
+	}
+
+	private String doActionDoWrite(HttpServletRequest req, HttpServletResponse resp) {
+		return null;
+	}
+
+	private String doActionDetail(HttpServletRequest req, HttpServletResponse resp) {
+		if (Util.empty(req, "id")) {
+			return "plain:id를 입력해주세요.";
+		}
+
+		if (Util.isNum(req, "id") == false) {
+			return "plain:id를 정수로 입력해주세요.";
+		}
+
+		int id = Util.getInt(req, "id");
+
+		Article article = articleService.getForPrintArticle(id);
+
+		req.setAttribute("article", article);
+
+		return "article/detail.jsp";
 	}
 
 	private String doActionList(HttpServletRequest req, HttpServletResponse resp) {
@@ -46,6 +73,6 @@ public class ArticleController extends Controller {
 
 		List<Article> articles = articleService.getForPrintListArticles(page, itemsInAPage, cateItemId);
 		req.setAttribute("articles", articles);
-		return "article/list";
+		return "article/list.jsp";
 	}
 }
